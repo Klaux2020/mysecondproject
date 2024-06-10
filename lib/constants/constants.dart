0e48cff1fa8d31cbc00372820dd0bd82211ab987
Klaux2020 <pervaldez.netscape.net>
2024-06-09 20:57:17 -0500

    Incremento usando funciones  Stateful

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const titulo = TextStyle(
+    fontSize: 60,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mysecondproject/constants/constants.dart';
 
 void main() {
   runApp(
@@ -15,11 +16,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int x = 100;
+  int x = 0;
+  int y = 0;
 
-   void IncrementarEn100(){
+   void IncrementarEn100XeY(){
      x += 100 ;
+     y += 100 ;
      setState(() {});
+  }
+
+   int Incrementar100(int variable){
+     return variable += 100 ;
   }
 
   @override
@@ -32,27 +39,70 @@ class _HomePageState extends State<HomePage> {
          child: Column(
            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-               Text(
-               x.toString(),
-                style: TextStyle(
-                  fontSize: 60,
-                  
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: 
+                [
+                  Column(
+                    children: [Text(
+                      "X",
+                      style: titulo,
+                      ), 
+                      Text(
+                      x.toString(),
+                      style:titulo,
+                      ), 
+                     ],
+                    ),
+                    VerticalDivider(
+                      width: 28
+                      ),
+                   Column(
+                    children: [ Text(
+                      "Y",
+                      style: titulo,
+                      ),
+                      Text(
+                      y.toString(),
+                      style:titulo,
+                    ),  
+                     ],
+                    ),
+                  ],
+                  ),
+                  Divider(
+                    thickness: 3,
+                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                                onPressed: (){
+                                 x=Incrementar100(x);
+                                setState(() {});
+                                }, 
+                                child: Text(
+                                  "Cambiar a x",
+                                  ),
+                                  ),
+                                     ElevatedButton(
+                                onPressed: (){
+                                 y=Incrementar100(y);
+                                setState(() {});
+                                }, 
+                                child: Text(
+                                  "Cambiar a y",
+                                  ),
+                                  ),
+                  ],
                 ),
-                ),      
-          ElevatedButton(
-            onPressed: (){
-             IncrementarEn100();
-            }, 
-            child: Text(
-              "Cambiar a 200",
-              ),
-              ),
             ElevatedButton(
             onPressed: (){
               print("el valor de x es : ${x.toString()}");
+              IncrementarEn100XeY();
             }, 
             child: Text(
-              "Print X",
+              "Cambiar ambos",
               ),
               ),
           ],
